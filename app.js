c5b737b230b16557f40f8d501ce689570d8a2da1
Emmanuel Oluwasheunn <138207984+Devsheunn@users.noreply.github.com>
2023-09-10 13:37:09 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const daysOfTheWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+//function expression to update time
+const updateTime = function () {
+  const current = new Date().getTime();
+  document.querySelector(".UTCInMilliseconds").textContent = `${current} (UTC)`;
+
+  const day = new Date().getDay();
+  document.querySelector(".dayOfTheWeek").textContent = daysOfTheWeek[day];
+};
+
+//using setInterval function to update time every 1000ms(1s)
+function displayCurrentUTC() {
+  setInterval(updateTime, 1000);
+}
+displayCurrentUTC();
