604b4c8f98d1a9c67a924f581966f971c313c035
Emmanuel Oluwasheunn <138207984+Devsheunn@users.noreply.github.com>
2023-09-11 09:35:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,14 +12,13 @@ const day = new Date().getDay();
 document.querySelector(".dayOfTheWeek").textContent = daysOfTheWeek[day];
 
 //function expression to update time
+
+const current = new Date().getTime();
+document.querySelector(".UTCInMilliseconds").textContent = current;
+
 const updateTime = function () {
   const current = new Date().getTime();
   document.querySelector(".UTCInMilliseconds").textContent = current;
 };
 
-//using setInterval function to update time every 1000ms(1s)
-function displayCurrentUTC() {
-  setInterval(updateTime, 1000);
-}
-
-displayCurrentUTC();
+setInterval(updateTime, 1000);
